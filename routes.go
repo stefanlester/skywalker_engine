b708bbd7e73b1a091845c10293f99fc3297c210d
Stefan <stefanamoah001@gmail.com>
2022-09-10 17:18:13 +0200

    feat: implemet sessions in skywalker

--- routes.go
+++ routes.go
@@ -11,6 +11,8 @@ func (a *application) routes() *chi.Mux {
 
 	// routing logic
 	a.App.Routes.Get("/", a.Handlers.Home)
+	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
+	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 
 	a.App.Routes.Get("/jet", func(w http.ResponseWriter, r *http.Request){
 		a.App.Render.JetPage(w, r, "testjet", nil, nil)
