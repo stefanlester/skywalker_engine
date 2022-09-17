9f1e01d5ce5a1637d559b5697d87f33661846d61
Stefan <stefanamoah001@gmail.com>
2022-09-17 23:38:54 +0200

    read data from the seeion and pass it to the jet template

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/CloudyKit/jet/v6"
 	"github.com/stefanlester/skywalker"
 )
 
@@ -26,6 +27,23 @@ func (h *Handlers) GoPage(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.JetPage(w, r, "jet-template", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
+
+func (h *Handlers) SessionTest(w http.ResponseWriter, r *http.Request) {
+	myData := "bar"
+
+	//set session
+	h.App.Session.Put(r.Context(), "foo", myData)
+
+	myValue := h.App.Session.GetString(r.Context(), "foo")
+
+	vars := make(jet.VarMap)
+	vars.Set("foo", myValue)
+
+	err := h.App.Render.JetPage(w, r, "sessions", vars, nil)
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
 	}
