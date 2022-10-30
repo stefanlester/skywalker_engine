99effc433e15b4c5ad267a4a5550fb6af2d43397
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-10-30 20:48:18 +0100

    testign database connection

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -15,8 +16,20 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
 
-	a.App.Routes.Get("/jet", func(w http.ResponseWriter, r *http.Request){
-		a.App.Render.JetPage(w, r, "testjet", nil, nil)
+	a.App.Routes.Get("test-database/jet", func(w http.ResponseWriter, r *http.Request) {
+		query := "select id, first_name from users where id = 1"
+		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
+
+		var id int
+		var name string
+
+		err := row.Scan(&id, &name)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+		}
+
+		fmt.Fprintf(w, "%d %s", id, name)
+	
 	})
 
 	// static routes
@@ -25,4 +38,3 @@ func (a *application) routes() *chi.Mux {
 
 	return a.App.Routes
 }
- 
