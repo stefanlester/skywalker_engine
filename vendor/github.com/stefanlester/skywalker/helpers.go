c1c1ee5bb14e3f6a2aa4dde3425de72d9503cfd7
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-06-17 19:55:14 +0200

    changes from skywalker

--- vendor/github.com/stefanlester/skywalker/helpers.go
+++ vendor/github.com/stefanlester/skywalker/helpers.go
@@ -1,7 +1,11 @@
 package skywalker
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
+	"encoding/base64"
+	"io"
 	"os"
 )
 
@@ -52,4 +56,49 @@ func (c *Skywalker) CreateFileIfNotExist(path string) error {
 	}
 
 	return nil
+}
+
+type Encryption struct {
+	Key []byte
+}
+
+func (e *Encryption) Encrypt(text string) (string, error) {
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(e.Key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+func (e *Encryption) Decrypt(cryptoText string) (string, error) {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(e.Key)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return "", err
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return string(ciphertext), nil
 }
--- vendor/github.com/stefanlester/skywalker/skywalker.go
+++ vendor/github.com/stefanlester/skywalker/skywalker.go
@@ -6,33 +6,58 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/alexedwards/scs/v2"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/go-chi/chi/v5"
+	"github.com/gomodule/redigo/redis"
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
+	"github.com/stefanlester/skywalker/cache"
+	"github.com/stefanlester/skywalker/filesystems/miniofilesystem"
+	"github.com/stefanlester/skywalker/mailer"
 	"github.com/stefanlester/skywalker/render"
 	"github.com/stefanlester/skywalker/session"
 )
 
 const version = "1.0.0"
 
+var myRedisCache *cache.RedisCache
+var myBadgerCache *cache.BadgerCache
+var redisPool *redis.Pool
+var badgerConn *badger.DB
+
 // Skywalker is the overall type for the Skywalker package. Members that are exported in this type are
 // are available to any application that uses it
 type Skywalker struct {
-	AppName  string
-	Debug    bool
-	Version  string
-	ErrorLog *log.Logger
-	InfoLog  *log.Logger
-	RootPath string
-	Routes   *chi.Mux
-	Render   *render.Render // render is a pointer to the render package
-	Session  *scs.SessionManager
-	DB       Database
-	JetViews *jet.Set
-	config   config
+	AppName       string
+	Debug         bool
+	Version       string
+	ErrorLog      *log.Logger
+	InfoLog       *log.Logger
+	RootPath      string
+	Routes        *chi.Mux
+	Render        *render.Render // render is a pointer to the render package
+	Session       *scs.SessionManager
+	DB            Database
+	JetViews      *jet.Set
+	config        config
+	EncryptionKey string
+	Cache         cache.Cache
+	Scheduler     *cron.Cron
+	Mail          mailer.Mail
+	Server        Server
+	FileSystems   map[string]interface{}
+}
+
+type Server struct {
+	ServerName string
+	Port       string
+	Secure     bool
+	URL        string
 }
 
 type config struct {
@@ -41,20 +66,23 @@ type config struct {
 	cookie      cookieConfig
 	sessionType string
 	database    databaseConfig
+	redis       redisConfig
 }
 
-func (c *Skywalker) New(rootPath string) error {
+// New reads the .env file, creates our application config, populates the Skywalker type with settings
+// based on .env values, and creates necessary folders and files if they don't exist
+func (s *Skywalker) New(rootPath string) error {
 	pathConfig := initPaths{
 		rootPath:    rootPath,
 		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
 	}
 
-	err := c.Init(pathConfig)
-	if err != nil { 
+	err := s.Init(pathConfig)
+	if err != nil {
 		return err
 	}
 
-	err = c.checkDotEnv(rootPath)
+	err = s.checkDotEnv(rootPath)
 	if err != nil {
 		return err
 	}
@@ -66,31 +94,54 @@ func (c *Skywalker) New(rootPath string) error {
 	}
 
 	// create loggers
-	infoLog, errorLog := c.startLoggers()
+	infoLog, errorLog := s.startLoggers()
 
 	//connect to database
 	if os.Getenv("DATABASE_TYPE") != "" {
-		db, err := c.OpenDB(os.Getenv("DATABASE_TYPE"), c.BuildDSN())
+		db, err := s.OpenDB(os.Getenv("DATABASE_TYPE"), s.BuildDSN())
 		if err != nil {
 			errorLog.Println(err)
 			os.Exit(1)
 		}
 
-		c.DB = Database{
+		s.DB = Database{
 			DataType: os.Getenv("DATABASE_TYPE"),
 			Pool:     db,
 		}
 
 	}
 
-	c.InfoLog = infoLog
-	c.ErrorLog = errorLog
-	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
-	c.Version = version
-	c.RootPath = rootPath
-	c.Routes = c.routes().(*chi.Mux)
+	scheduler := cron.New()
+	s.Scheduler = scheduler
 
-	c.config = config{
+	if os.Getenv("CACHE") == "redis" || os.Getenv("SESSION_TYPE") == "redis" {
+		myRedisCache = s.createClientRedisCache()
+		s.Cache = myRedisCache
+		redisPool = myRedisCache.Conn
+	}
+
+	if os.Getenv("CACHE") == "badger" {
+		myBadgerCache = s.createClientBadgerCache()
+		s.Cache = myBadgerCache
+		badgerConn = myBadgerCache.Conn
+
+		_, err = s.Scheduler.AddFunc("@daily", func() {
+			_ = myBadgerCache.Conn.RunValueLogGC(0.7)
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	s.InfoLog = infoLog
+	s.ErrorLog = errorLog
+	s.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	s.Version = version
+	s.RootPath = rootPath
+	s.Mail = s.createMailer()
+	s.Routes = s.routes().(*chi.Mux)
+
+	s.config = config{
 		port:     os.Getenv("PORT"),
 		renderer: os.Getenv("RENDERER"),
 		cookie: cookieConfig{
@@ -103,39 +154,72 @@ func (c *Skywalker) New(rootPath string) error {
 		sessionType: os.Getenv("SESSION_TYPE"),
 		database: databaseConfig{
 			database: os.Getenv("DATABASE_TYPE"),
-			dsn:      c.BuildDSN(),
+			dsn:      s.BuildDSN(),
 		},
+		redis: redisConfig{
+			host:     os.Getenv("REDIS_HOST"),
+			password: os.Getenv("REDIS_PASSWORD"),
+			prefix:   os.Getenv("REDIS_PREFIX"),
+		},
+	}
+
+	secure := true
+	if strings.ToLower(os.Getenv("SECURE")) == "false" {
+		secure = false
+	}
+
+	s.Server = Server{
+		ServerName: os.Getenv("SERVER_NAME"),
+		Port:       os.Getenv("PORT"),
+		Secure:     secure,
+		URL:        os.Getenv("APP_URL"),
 	}
 
 	// create session manager
 	session := session.Session{
-		CookieLifetime: c.config.cookie.lifetime,
-		CookiePersist:  c.config.cookie.persist,
-		CookieName:     c.config.cookie.name,
-		SessionType:    c.config.sessionType,
-		CookieDomain:   c.config.cookie.domain,
+		CookieLifetime: s.config.cookie.lifetime,
+		CookiePersist:  s.config.cookie.persist,
+		CookieName:     s.config.cookie.name,
+		SessionType:    s.config.sessionType,
+		CookieDomain:   s.config.cookie.domain,
 	}
 
-	c.Session = session.InitSession()
+	switch s.config.sessionType {
+	case "redis":
+		session.RedisPool = myRedisCache.Conn
+	case "mysql", "postgres", "mariadb", "postgresql":
+		session.DBPool = s.DB.Pool
+	}
 
-	var views = jet.NewSet(
-		jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
-		jet.InDevelopmentMode(),
-	)
+	s.Session = session.InitSession()
+	s.EncryptionKey = os.Getenv("KEY")
 
-	c.JetViews = views
+	if s.Debug {
+		var views = jet.NewSet(
+			jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
+			jet.InDevelopmentMode(),
+		)
+		s.JetViews = views
+	} else {
+		var views = jet.NewSet(
+			jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
+		)
+		s.JetViews = views
+	}
 
-	c.createRenderer()
+	s.createRenderer()
+	s.FileSystems = s.createFileSystems()
+	go s.Mail.ListenForMail()
 
 	return nil
 }
 
 // Init creates necessary folders for our Skywalker application
-func (c *Skywalker) Init(p initPaths) error {
+func (s *Skywalker) Init(p initPaths) error {
 	root := p.rootPath
 	for _, path := range p.folderNames {
 		// create a folder if it doesn't exist
-		err := c.CreateDirIfNotExist(root + "/" + path)
+		err := s.CreateDirIfNotExist(root + "/" + path)
 		if err != nil {
 			return err
 		}
@@ -144,26 +228,35 @@ func (c *Skywalker) Init(p initPaths) error {
 }
 
 // ListenAndServe starts the web server
-func (c *Skywalker) ListenAndServe() {
+func (s *Skywalker) ListenAndServe() {
 	serve := &http.Server{
 		Addr:         fmt.Sprintf(":%s", os.Getenv("PORT")),
-		ErrorLog:     c.ErrorLog,
-		Handler:      c.Routes,
+		ErrorLog:     s.ErrorLog,
+		Handler:      s.Routes,
 		IdleTimeout:  30 * time.Second,
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 600 * time.Second,
 	}
 
-	// Close database connection pool
-	defer c.DB.Pool.Close()
+	if s.DB.Pool != nil {
+		defer s.DB.Pool.Close()
+	}
 
-	c.InfoLog.Printf("Starting server on port %s", os.Getenv("PORT"))
+	if redisPool != nil {
+		defer redisPool.Close()
+	}
+
+	if badgerConn != nil {
+		defer badgerConn.Close()
+	}
+
+	s.InfoLog.Printf("Listening on port %s", os.Getenv("PORT"))
 	err := serve.ListenAndServe()
-	c.ErrorLog.Fatal(err)
+	s.ErrorLog.Fatal(err)
 }
 
-func (c *Skywalker) checkDotEnv(path string) error {
-	err := c.CreateFileIfNotExist(fmt.Sprintf("%s/.env", path))
+func (s *Skywalker) checkDotEnv(path string) error {
+	err := s.CreateFileIfNotExist(fmt.Sprintf("%s/.env", path))
 	if err != nil {
 		return err
 	}
@@ -180,16 +273,78 @@ func (c *Skywalker) startLoggers() (*log.Logger, *log.Logger) {
 	return infoLog, errorLog
 }
 
-func (c *Skywalker) createRenderer() {
+func (s *Skywalker) createRenderer() {
 	myRenderer := render.Render{
-		Renderer: c.config.renderer,
-		RootPath: c.RootPath,
-		Port:     c.config.port,
-		JetViews: c.JetViews,
-		Session: c.Session,
+		Renderer: s.config.renderer,
+		RootPath: s.RootPath,
+		Port:     s.config.port,
+		JetViews: s.JetViews,
+		Session:  s.Session,
 	}
 
-	c.Render = &myRenderer
+	s.Render = &myRenderer
+}
+
+func (s *Skywalker) createMailer() mailer.Mail {
+	port, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	m := mailer.Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Templates:   s.RootPath + "/mail",
+		Host:        os.Getenv("SMTP_HOST"),
+		Port:        port,
+		Username:    os.Getenv("SMTP_USERNAME"),
+		Password:    os.Getenv("SMTP_PASSWORD"),
+		Encryption:  os.Getenv("SMTP_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+		Jobs:        make(chan mailer.Message, 20),
+		Results:     make(chan mailer.Result, 20),
+		API:         os.Getenv("MAILER_API"),
+		APIKey:      os.Getenv("MAILER_KEY"),
+		APIUrl:      os.Getenv("MAILER_URL"),
+	}
+	return m
+}
+
+func (s *Skywalker) createClientRedisCache() *cache.RedisCache {
+	cacheClient := cache.RedisCache{
+		Conn:   s.createRedisPool(),
+		Prefix: s.config.redis.prefix,
+	}
+	return &cacheClient
+}
+
+func (s *Skywalker) createClientBadgerCache() *cache.BadgerCache {
+	cacheClient := cache.BadgerCache{
+		Conn: s.createBadgerConn(),
+	}
+	return &cacheClient
+}
+
+func (s *Skywalker) createRedisPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     50,
+		MaxActive:   10000,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp",
+				s.config.redis.host,
+				redis.DialPassword(s.config.redis.password))
+		},
+
+		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
+			_, err := conn.Do("PING")
+			return err
+		},
+	}
+}
+
+func (s *Skywalker) createBadgerConn() *badger.DB {
+	db, err := badger.Open(badger.DefaultOptions(s.RootPath + "/tmp/badger"))
+	if err != nil {
+		return nil
+	}
+	return db
 }
 
 func (c *Skywalker) BuildDSN() string {
@@ -204,13 +359,46 @@ func (c *Skywalker) BuildDSN() string {
 			os.Getenv("DATABASE_NAME"),
 			os.Getenv("DATABASE_SSL_MODE"))
 
+		// we check to see if a database password has been supplied, since including "password=" with nothing
+		// after it sometimes causes postgres to fail to allow a connection.
 		if os.Getenv("DATABASE_PASS") != "" {
 			dsn = fmt.Sprintf("%s password=%s", dsn, os.Getenv("DATABASE_PASS"))
 		}
+
+	case "mysql", "mariadb":
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?collation=utf8_unicode_ci&timeout=5s&parseTime=true&tls=%s&readTimeout=5s",
+			os.Getenv("DATABASE_USER"),
+			os.Getenv("DATABASE_PASS"),
+			os.Getenv("DATABASE_HOST"),
+			os.Getenv("DATABASE_PORT"),
+			os.Getenv("DATABASE_NAME"),
+			os.Getenv("DATABASE_SSL_MODE"))
 
 	default:
 
 	}
 
 	return dsn
+}
+
+func (s *Skywalker) createFileSystems() map[string]interface{} {
+	fileSystems := make(map[string]interface{})
+
+	if os.Getenv("MINIO_SECRET") != "" {
+		useSSL := false
+		if strings.ToLower(os.Getenv("MINIO_USESSL")) == "true" {
+			useSSL = true
+		}
+
+		minio := miniofilesystem.Minio{
+			Endpoint: os.Getenv("MINIO_ENDPOINT"),
+			Key:      os.Getenv("MINIO_KEY"),
+			Secret:   os.Getenv("MINIO_SECRET"),
+			UseSSL:   useSSL,
+			Region:   os.Getenv("MINIO_REGION"),
+			Bucket:   os.Getenv("MINIO_BUCKET"),
+		}
+		fileSystems["MINIO"] = minio
+	}
+	return fileSystems
 }
