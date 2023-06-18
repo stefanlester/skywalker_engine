f04b8ed494253d36d3ebb27dfd5199cfffd4144a
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-06-18 16:08:31 +0200

    refactor handles; handlers give access to Skywalker and models

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,51 +1,20 @@
 package handlers
 
 import (
-	"net/http"
-	"myapp/data"
-
-	"github.com/CloudyKit/jet/v6"
 	"github.com/stefanlester/skywalker"
+	"myapp/data"
+	"net/http"
 )
 
+// Handlers is the type for handlers, and gives access to Celeritas and models
 type Handlers struct {
 	App    *skywalker.Skywalker
 	Models data.Models
 }
 
+// Home is the handler to render the home page
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
-	err := h.App.Render.Page(w, r, "home", nil, nil)
-	if err != nil {
-		h.App.ErrorLog.Println("error rendering:", err)
-	}
-}
-
-func (h *Handlers) GoPage(w http.ResponseWriter, r *http.Request) {
-	err := h.App.Render.GoPage(w, r, "home", nil)
-	if err != nil {
-		h.App.ErrorLog.Println("error rendering:", err)
-	}
-}
-
-func (h *Handlers) JetPage(w http.ResponseWriter, r *http.Request) {
-	err := h.App.Render.JetPage(w, r, "jet-template", nil, nil)
-	if err != nil {
-		h.App.ErrorLog.Println("error rendering:", err)
-	}
-}
-
-func (h *Handlers) SessionTest(w http.ResponseWriter, r *http.Request) {
-	myData := "bar"
-
-	//set session
-	h.App.Session.Put(r.Context(), "foo", myData)
-
-	myValue := h.App.Session.GetString(r.Context(), "foo")
-
-	vars := make(jet.VarMap)
-	vars.Set("foo", myValue)
-
-	err := h.App.Render.JetPage(w, r, "sessions", vars, nil)
+	err := h.render(w, r, "home", nil, nil)
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
 	}
