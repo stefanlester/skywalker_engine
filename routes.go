82a6e16da3c4ec3bcb1e2ee17c55dbd7f55b7561
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-06-18 15:01:31 +0200

    refactor and modularize routes

--- routes.go
+++ routes.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"myapp/data"
+	"log"
 	"net/http"
-	"strconv"
+
+	"github.com/stefanlester/skywalker/filesystems/miniofilesystem"
+
+	"github.com/stefanlester/skywalker"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -13,78 +15,28 @@ func (a *application) routes() *chi.Mux {
 	// middleware must come before any routes
 
 	// add routes here
-	a.App.Routes.Get("/", a.Handlers.Home)
-	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
-	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
-	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+	a.get("/", a.Handlers.Home)
+	a.get("/test-minio", func(w http.ResponseWriter, r *http.Request) {
+		f := a.App.FileSystems["MINIO"].(miniofilesystem.Minio)
 
-	a.App.Routes.Get("/users/login", a.Handlers.UserLogin)
-	a.App.Routes.Post("/users/login", a.Handlers.PostUserLogin)
-	a.App.Routes.Get("/users/logout", a.Handlers.Logout)
-
-	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
-		u := data.User{
-			FirstName: "Trevor",
-			LastName:  "Sawler",
-			Email:     "me@here.com",
-			Active:    1,
-			Password:  "password",
-		}
-
-		id, err := a.Models.Users.Insert(u)
+		files, err := f.List("")
 		if err != nil {
-			a.App.ErrorLog.Println(err)
+			log.Println(err)
 			return
 		}
 
-		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
-	})
-
-	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) {
-		users, err := a.Models.Users.GetAll()
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
+		for _, file := range files {
+			log.Println(file.Key)
 		}
-		for _, x := range users {
-			fmt.Fprint(w, x.LastName)
-		}
-	})
-
-	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
-
-		u, err := a.Models.Users.Get(id)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
-	})
-
-	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
-		u, err := a.Models.Users.Get(id)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		u.LastName = a.App.RandomString(10)
-		err = u.Update(*u)
-		if err != nil {
-			a.App.ErrorLog.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "updated last name to %s", u.LastName)
-
 	})
 
 	// static routes
 	fileServer := http.FileServer(http.Dir("./public"))
 	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
+
+	// routes from celeritas
+	a.App.Routes.Mount("/skywalker", skywalker.Routes())
+	a.App.Routes.Mount("/api", a.ApiRoutes())
 
 	return a.App.Routes
 }
