da2a5116ac51048791865bd355b2a08e1fc9cf73
lordmarasmus <145048142+lordmarasmus@users.noreply.github.com>
2023-10-27 12:45:24 -0700

    m

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -25,6 +25,7 @@ func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// List File System Handler
 func (h *Handlers) ListFS(w http.ResponseWriter, r *http.Request) {
 	var fs filesystems.FS
 	var list []filesystems.Listing
